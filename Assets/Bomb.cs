32de68c16be98662611249533934bd07607d9f4a
vanhoang2803 <vanhoang2803@users.noreply.github.com>
2020-11-25 00:03:40 +0700

    Basic map, character animation, planting bomb, bomb animation, character movement

---
+++ Assets/Bomb.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private int frame = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frame += 1;
+        if (frame == 240*4)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/BombPlanting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+
+public class BombPlanting : MonoBehaviour
+{
+    public Transform Player;
+    public GameObject bombPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.Space)){
+            PlantBomb();
+        }
+    }
+    void PlantBomb() {
+        Vector3 bombPosition = Player.position;
+        bombPosition.x = Mathf.Round(bombPosition.x); 
+        bombPosition.y = Mathf.Round(bombPosition.y); 
+        bombPosition.z = Mathf.Round(bombPosition.z); 
+        Instantiate(bombPrefab,bombPosition,Player.rotation);
+    }
+    
+}
