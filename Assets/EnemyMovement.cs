c3d6158e6d9fa45e433497c6fc1327146856141c
vanhoang2803 <vanhoang2803@users.noreply.github.com>
2021-01-08 14:02:10 +0700

    Pathfinding enemy

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public AIPath aiPath;
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = aiPath.desiredVelocity.x;
+        movement.y = aiPath.desiredVelocity.y;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    void FixedUpdate() {
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+
