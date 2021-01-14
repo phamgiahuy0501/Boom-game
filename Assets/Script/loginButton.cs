d5fb747430f3bde66398b1d3246a02816ae34881
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2021-01-15 01:04:31 +0700

    Update
    add raw UI login

---
+++ Assets/Script/loginButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loginButton : MonoBehaviour
+{
+    public GameObject usernameField;
+    public GameObject passwordField;
+
+    private string constUser = "Admin";
+    private string constPass = "Admin";
+    public void process()
+    {
+        Debug.Log(usernameField.GetComponent<InputField>().text);
+        Debug.Log(passwordField.GetComponent<InputField>().text);
+        Debug.Log("ok");
+    }
+}
